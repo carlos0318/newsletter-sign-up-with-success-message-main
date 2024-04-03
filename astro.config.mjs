67651573f2d3b94bb9a6e22f1e61873375f18974
Carlos Ortega <carlos.ortega.ast0318@gmail.com>
2024-04-03 16:42:45 -0600

    add yml to upload page

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,12 @@ import tailwind from "@astrojs/tailwind";
 
 import react from "@astrojs/react";
 
+const LIVE_URL = 'https://carlos0318.github.io'
+const BASE = 'newsletter-sign-up-with-success-message-main'
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()]
+  integrations: [tailwind(), react()],
+  site: LIVE_URL,
+  base: BASE
 });
